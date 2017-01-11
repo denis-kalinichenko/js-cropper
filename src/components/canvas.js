ff8c17ed80bc0c2f20e3b54921a2acde9c128e91
Denis Kalinichenko <denis@kalinichenko.me>
2017-01-11 22:02:48 +0100

    GN-57 render basic canvas

---
+++ src/components/canvas.js
@@ -0,0 +1,12 @@
+import Element from "./element";
+
+/**
+ * Class representing a canvas element
+ *
+ */
+
+export default class Canvas extends Element {
+    constructor() {
+        super("canvas");
+    }
+}
--- src/main/index.js
+++ src/main/index.js
@@ -1,8 +1,20 @@
+import { validateNode } from "./../utils/validators";
+import Canvas from "./../components/canvas";
+
 /**
  * Image Crop module
  *
  */
 
 export default class ImageCrop {
+    constructor() {
 
+    }
+
+    render(node) {
+        this._node = validateNode(node);
+        this._canvas = new Canvas();
+        this._canvas.render(this._node);
+        return this;
+    }
 }
