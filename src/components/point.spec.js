fadbefb3dc5d7f921741cd8cc07bff5f7b2fa737
Denis Kalinichenko <denis@kalinichenko.me>
2017-01-19 11:33:31 +0100

    Point component

---
+++ src/components/point.spec.js
@@ -0,0 +1,16 @@
+import { expect } from "chai";
+import Point from "./point";
+
+describe("Point component",() => {
+    let point;
+
+    it("initialises", () => {
+        expect(() => {new Point()}).to.not.throw(Error);
+    });
+
+    it("creates Point element", () => {
+        point = new Point(50, 300);
+        expect(point.x).to.equal(50);
+        expect(point.y).to.equal(300);
+    });
+});
