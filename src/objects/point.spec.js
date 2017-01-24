744b533d51cc900edf2dffdf18b80a94bb42e847
Denis Kalinichenko <denis@kalinichenko.me>
2017-01-25 00:56:29 +0100

    removed size, point from tests scope

--- src/objects/point.spec.js
+++ src/objects/point.spec.js
@@ -2,14 +2,12 @@ import { expect } from "chai";
 import Point from "./point";
 
 describe("Point component",() => {
-    let point;
-
     it("initialises", () => {
         expect(() => {new Point()}).to.not.throw();
     });
 
     it("creates Point element", () => {
-        point = new Point(50, 300);
+        const point = new Point(50, 300);
         expect(point.x).to.equal(50);
         expect(point.y).to.equal(300);
     });
--- src/objects/size.spec.js
+++ src/objects/size.spec.js
@@ -2,14 +2,12 @@ import { expect } from "chai";
 import Size from "./size";
 
 describe("Size component",() => {
-    let size;
-
     it("initialises", () => {
         expect(() => {new Size()}).to.not.throw();
     });
 
     it("creates Size element", () => {
-        size = new Size(50, 300);
+        const size = new Size(50, 300);
         expect(size.width).to.equal(50);
         expect(size.height).to.equal(300);
     });
